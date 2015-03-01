d3dd17fc35f58f7191ce2143d5f274bc980272a5
Randy Gelhausen <rgelhau@gmail.com>
2015-03-01 02:17:15 -0500

    Added comments

--- src/com/github/randerzander/StreamSimulator.java
+++ src/com/github/randerzander/StreamSimulator.java
@@ -26,13 +26,14 @@ public class StreamSimulator {
     SimpleDateFormat dateFormat = (args.length == 7) ? new SimpleDateFormat(args[6]) : new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
 
     Properties props = new Properties();
-    props.put("metadata.broker.list", host+":6667");
+    props.put("metadata.broker.list", host+":6667"); //HDP 2.2 Kafka uses port 6667 by default
     props.put("serializer.class", "kafka.serializer.StringEncoder");
-    props.put("request.required.acks", "1");
+    props.put("request.required.acks", "1"); //Using synchronous message production
     ProducerConfig config = new ProducerConfig(props);
     Producer<String, String> producer = new Producer<String, String>(config);
     File[] files = new File(input).listFiles();
 
+    //For each file in the directory, read it and feed it to Kafka line by line
     for(File file : files){
       try{
         String path = input + file.getName();
@@ -45,13 +46,15 @@ public class StreamSimulator {
         long prevTimestamp = 0l;
         while ((line = br.readLine()) != null) {
           if (speed == -1){
+            //Split line into columns to extract a timestamp
             String ts = line.split(args[4])[Integer.parseInt(args[5])];
+            //Attempt to parse into date time and compute time delta from last timestamp
             long currentTimestamp = dateFormat.parse(ts).getTime();
+            //Sleep for the delta
             if (prevTimestamp != 0) Thread.sleep(currentTimestamp - prevTimestamp);
-            System.out.println("Slept " + Long.toString(currentTimestamp - prevTimestamp));
             prevTimestamp = currentTimestamp;
           }else if (speed > 0) Thread.sleep(speed);
-          System.out.println(topic+":"+host+":" + line);
+          System.out.println("Sending: " + topic+":"+host+":" + line);
           producer.send(new KeyedMessage<String, String>(topic, line));
         }
         System.out.println("Finished ingesting file: " + path);
